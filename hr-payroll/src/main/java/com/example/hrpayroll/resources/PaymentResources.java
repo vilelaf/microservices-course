4f3b0bf223eef4fea314d85337395af41f0db8cd
fabiovilela <fabiovilela@ibm.com>
2023-02-01 13:53:18 -0300

    hr-payroll mock service

---
+++ hr-payroll/src/main/java/com/example/hrpayroll/resources/PaymentResources.java
@@ -0,0 +1,29 @@
+package com.example.hrpayroll.resources;
+
+import com.example.hrpayroll.entities.Payment;
+import com.example.hrpayroll.services.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/payments")
+public class PaymentResources {
+
+    @Autowired
+    private PaymentService service;
+
+    @GetMapping(value = "/{workerId}/days/{days}")
+    public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable int days){
+
+            Payment payment = service.getPayment(workerId, days);
+            return ResponseEntity.ok(payment);
+
+    }
+
+
+
+}
