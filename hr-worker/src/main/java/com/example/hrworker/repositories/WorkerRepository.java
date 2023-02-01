3d6b2cdd9584299602dbb93f8349facfe0c0d77f
fabiovilela <fabiovilela@ibm.com>
2023-02-01 20:23:02 -0300

    Ajeitando uma bagunça

---
+++ hr-worker/src/main/java/com/example/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,11 @@
+package com.example.hrworker.repositories;
+
+
+import com.example.hrworker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+
+}
