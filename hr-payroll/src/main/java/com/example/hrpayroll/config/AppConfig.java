3c36d785f6eaef10a7859c464757e388cb89dd8b
fabiovilela <fabiovilela@ibm.com>
2023-02-01 21:49:31 -0300

    Feing CLient

--- hr-payroll/src/main/java/com/example/hrpayroll/config/AppConfig.java
+++ hr-payroll/src/main/java/com/example/hrpayroll/config/AppConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
 public class AppConfig {
 
     @Bean
-    public RestTemplate getRestTemplate(){
+    public RestTemplate getRestTemplate() {
 
         return new RestTemplate();
 
