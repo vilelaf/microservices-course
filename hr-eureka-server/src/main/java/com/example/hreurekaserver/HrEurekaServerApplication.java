1f346b3e42c3c28385e930b4c490d69e7398ca31
fabiovilela <fabiovilela@ibm.com>
2023-02-02 13:38:04 -0300

    Eureka Starts Ok

--- hr-eureka-server/src/main/java/com/example/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/example/hreurekaserver/HrEurekaServerApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HrEurekaServerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HrEurekaServerApplication.class, args);
+
 	}
 
 }
