78f1443d36b45997dfc14202f9f82fc9d525b438
fabiovilela <fabiovilela@ibm.com>
2023-02-03 18:54:17 -0300

    hr user configurado

---
+++ hr-user/src/main/java/com/example/hruser/entities/Role.java
@@ -0,0 +1,52 @@
+package com.example.hruser.entities;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name= "tb_role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String roleName;
+
+    public Role() {
+    }
+
+    public Role(String id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return roleName.equals(role.roleName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roleName);
+    }
+}
