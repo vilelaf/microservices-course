2f0318dfb2211ee3f6045c008c5f310c0e3e2d72
fabiovilela <fabiovilela@ibm.com>
2023-02-03 20:40:40 -0300

    User Resorces Zull Parametrizado com User

---
+++ hr-user/src/main/java/com/example/hruser/resources/UserResource.java
@@ -0,0 +1,48 @@
+package com.example.hruser.resources;
+
+import com.example.hruser.entities.User;
+import com.example.hruser.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = repository.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+
+        User obj = repository.findById(id).get();
+        return ResponseEntity.ok(obj);
+    }
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<User> findByEmail(@RequestParam String email) {
+
+        User obj = repository.findByEmail(email);
+        return ResponseEntity.ok(obj);
+    }
+
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs() {
+        return ResponseEntity.noContent().build();
+    }
+
+}
