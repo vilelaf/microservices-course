5f2380ba018d2b3aa7ae06bbf2ee9602bd01c797
fabiovilela <fabiovilela@ibm.com>
2023-02-04 13:13:49 -0300

    Hr-Oauth implementado sem autenticação

---
+++ hr-oauth/src/main/java/com/example/hroauth/entities/Role.java
@@ -0,0 +1,48 @@
+package com.example.hroauth.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Role implements Serializable {
+
+    private String id;
+    private String roleName;
+
+    public Role() {
+    }
+
+    public Role(String id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return roleName.equals(role.roleName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roleName);
+    }
+}
